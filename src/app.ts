4fe97c968df17f392d52c938c0a569f0cea6fa22
Milan Navratil <navratil.milann@gmail.com>
2025-08-15 10:52:08 +0200

    [FIX] Removed GraphQL

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,6 @@
 import { failedResponse } from "./utils/api.ts";
 import { FRONTEND_ORIGIN } from "./constants.ts";
 import type { Request, Response, NextFunction } from "express";
-import graphqlSchema from "./graphql/schema.ts";
-import graphqlResolvers from "./graphql/resolvers.ts";
 
 import logger from "./plugins/logger.ts";
 import helmet from 'helmet'
@@ -12,8 +10,6 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import auth from "./middlewares/auth.ts";
-import { createHandler } from "graphql-http/lib/use/express";
-import { getErrorResponse } from "./utils/error.ts";
 
 const app = express();
 
@@ -35,18 +31,6 @@ app.use(
 );
 
 app.use(auth);
-
-app.all(
-  "/graphql",
-  createHandler({
-    schema: graphqlSchema,
-    rootValue: graphqlResolvers,
-    context: (req) => ({
-      user: req.raw.user,
-    }),
-    formatError: (err: any) => getErrorResponse(err),
-  })
-);
 
 // error handling
 app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
